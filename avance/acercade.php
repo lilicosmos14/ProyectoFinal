3b44dea2bbd62809b58eb060d1abebbcedabade6
Ariadna Judith <118841281+AriRosas14@users.noreply.github.com>
2022-12-08 19:15:04 -0600

    Add files via upload
    Nuevas paginas realizadas, y la de contacto ya tiene un mapa

---
+++ avance/acercade.php
@@ -0,0 +1,82 @@
+<?php
+    include "menu.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>MO'O REEKS</title>
+    <link rel="stylesheet" href="css/estilo.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/estilosIndex.css">
+    <link href="https://fonts.googleapis.com/css2?family=Rubik+Mono+One&family=Silkscreen&family=Stick+No+Bills:wght@800&display=swap" rel="stylesheet">
+</head>
+
+    <main>
+        <div class="main">
+            <article class="texto">
+                <h1 style="text-align: center;"> Acerca de nosotros </h1>
+                <p style="font-size:20px;">
+                        La certificación, es el proceso llevado a cabo por una entidad
+                        reconocida como independiente de las partes interesadas, mediante
+                        el que se manifiesta la conformidad de una determinada empresa,
+                        producto, proceso, servicio o persona con los requisitos
+                        definidos en normas o especificaciones técnicas
+                        Este es un examen creado para la certificacion de
+                        lenguaje de programacion en el cual te ayudara
+                        a tener mejores aspirciones y conseguir mejore trabajos.
+                        Tenemors años de experiencia, y nuestros testimonios lo acreditan,
+                        estamos posicionados dentro de las mejores empresas para certificacion
+                        en lenguajes de programacion
+                </p>
+            </article>
+            <nav class="imagen">
+                <img src="imagenes/logo.jpeg" width="500px;">
+            </nav>
+
+        </div>
+        <br><br><br>
+        <br>
+        <br>
+        <br>
+        <h2 style="text-align: center;">Creadores de la pagina</h2>
+        <section class="servicios">
+            <div class="a1">
+                <h4>Andres Noriega</h4>
+                <a href="imagenes/Andres%20Image%202022-10-21%20at%205.48.03%20PM.jpeg"></a><img src="imagenes/Andres%20Image%202022-10-21%20at%205.48.03%20PM.jpeg" width="200">
+
+            </div>
+            <div class="a2">
+                <h4>Ariadna Rosas</h4>
+                <a href="imagenes/Ari%20Image%202022-10-21%20at%205.46.34%20PM.jpeg"></a><img src="imagenes/Ari%20Image%202022-10-21%20at%205.46.34%20PM.jpeg" width="200">
+
+            </div>
+            <div class="a3">
+                <h4>Alejandro Duran</h4>
+                <a href="imagenes/Alex%20Image%202022-10-30%20at%201.44.50%20PM.jpeg"></a><img src="imagenes/Alex%20Image%202022-10-30%20at%201.44.50%20PM.jpeg" width="200">
+
+            </div>
+            <div class="a4">
+                <a href="imagenes/me%20Image%202022-10-21%20at%205.49.23%20PM.jpeg"></a>
+                <h4>Kenya García</h4><img src="imagenes/me%20Image%202022-10-21%20at%205.49.23%20PM.jpeg" width="200">
+
+            </div>
+        </section>
+
+    </main>
+
+    <?php
+    include "footer.html";
+    ?>
+
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    
+</body>
+
+</html>
