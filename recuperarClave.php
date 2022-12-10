c8ee6d4edaf23e6debe8437c83e80ea7e6d9eaad
Ariadna Judith <118841281+AriRosas14@users.noreply.github.com>
2022-12-10 16:58:45 -0600

    Update recuperarClave.php

--- recuperarClave.php
+++ recuperarClave.php
@@ -1,8 +1,3 @@
-<head>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
-</head>
-
 <?php
 $con = new mysqli("localhost","id19772720_root","^ypH0|r[n&i0/Pfx","id19772720_usuarios");
 if($con->connect_errno){
@@ -48,3 +43,5 @@ if($cantidadConsulta ==0){
 }
 
 ?>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
