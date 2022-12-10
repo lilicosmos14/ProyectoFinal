a2ff076e8a8961c9ef81fdfac5d0183598480556
Ariadna Judith <118841281+AriRosas14@users.noreply.github.com>
2022-12-10 16:34:43 -0600

    Update login.php
    Ultima actualizacion del archivo

--- avance/login.php
+++ avance/login.php
@@ -1,20 +1,6 @@
 <?php
     include "menu.php";
 ?>
-<?php
-function randomText($lenght){
-    $key="";
-    $pattern="1234567890abcdefghijklmnopqrstuvwxyz";
-    for($i=0;$i<$lenght;$i++){
-        $key .= $pattern[rand(0,35)];
-    }
-    return $key;
-}
-
-$captcha=randomText(6);
-//echo "captcha generado: ". $captcha;
-
-?>
 
 <head>
     <meta charset="UTF-8">
@@ -25,6 +11,7 @@ $captcha=randomText(6);
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <script src="js/bootstrap.min.js"></script>
     <script src="https://www.google.com/recaptcha/api.js" async defer></script>
+    <script src="js/load_captcha.js"></script>
 </head>
 <?php
     include ("msjs.php");
@@ -36,27 +23,30 @@ $captcha=randomText(6);
     <form class="form" action="verificar.php" method="post">
         <div class="container mt-3">
             <div class="form-group">
-                <input name="usuario" id="emailInput" placeholder="Usuario" class="form-control form-control-sm" type="text" required="" value="<?php if(isset($_COOKIE["usuario"])) {echo $_COOKIE["usuario"]; } ?>">
+                <input name="usuario" id="usuario" placeholder="Usuario" class="form-control form-control-sm" type="text" required="" value="<?php if(isset($_COOKIE["usuario"])) {echo $_COOKIE["usuario"]; } ?>">
             </div>
             <div class="form-group">
-                <input name="palabra_secreta" id="passwordInput" placeholder="Password" class="form-control form-control-sm" type="password" required="" value="<?php if(isset($_COOKIE["palabra_secreta"])) {echo $_COOKIE["palabra_secreta"]; } ?>">
+                <input name="palabra_secreta" id="palabra_secreta" placeholder="Password" class="form-control form-control-sm" type="password" required="" value="<?php if(isset($_COOKIE["palabra_secreta"])) {echo $_COOKIE["palabra_secreta"]; } ?>">
             </div>
-            <p>
-                Ingresar texto: <span id="captcha"><?php echo $captcha ?></span><br>
-                <input name="respuesta" type="text" sixe="6">
-                <input type="hidden" name="captcha" value="<?php echo $captcha ?>">
-            </p>
+            <div class="form-group">
+				<input type="text" name="securityCode" id="securityCode" class="form-control" placeholder="Código de seguridad">
+			</div>
+			<div class="form-group">								
+				<label class="col-md-4 control-label">	<img style="border: 1px solid #D3D0D0" src="get_captcha.php?rand=<?php echo rand(); ?>" id='captcha'></label>
+                <a href="javascript:void(0)" id="reloadCaptcha" class="btn btn-light">Recargar codigo</a>  
+            
+			</div>
             <p>
                 <input type="checkbox" name="remember" /> Recordar contraseña del usuario
             </p>
             <div class="mb-3" style="padding: left 15%;">
                 <div style="padding-left: 50px;" class="g-recaptcha" data-sitekey="6LeC3uEiAAAAAFnWU_cl8BrR19EMwAB9OFQKKutd">
                 </div>
-            </div>    
+            </div>     
             <div class="form-group">
                 <button type="submit" name="acceder" class="btn btn-primary btn-block">Iniciar Sesión</button>
                 <br>
-                <a href="recuperar.php" id="olvidar" title="Recuperar Clave">Recuperar Clave</a>
+                <a href="recuperar.php" id="olvidar" title="Recuperar Clave" class="btn btn-success">Recuperar Clave</a>
             </div>
         </div>    
     </form>
